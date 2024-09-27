13f97d540018587c03c3688b5b5af067d03bd0a4
Talent <talentpthomas@gmail.com>
2024-09-27 23:09:05 +0530

    episode-02

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
+....
 # React 🚀
+
+
+.....
+# Parcel
+- Dev Build
+- Local Server
+- HMR  = Hot Module Replacement
+- File Watching Algorithm - written in c++
+- Caching - Faster Builds
+- Imgae Optimization 
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differntial Bundling - support older browers
+- Diagnostic 
+- Error Handling 
+- HTTPs
+- Tree Shaking - remove unused code
+- Different Build for dev and production bundles
--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am an h1 element"),
+    React.createElement("h1", {}, "Namaste React🚀"),
     React.createElement("h2", {}, "I am an h2 element"),
   ]),
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am an h1 element"),
+    React.createElement("h1", {}, "This is Talent"),
     React.createElement("h2", {}, "I am an h2 element"),
   ])
 ]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(parent);
